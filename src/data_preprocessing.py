8a798d6297eeb6205b7391be0c47e588f876529f
afzalvfx <afzalcreatives@gmail.com>
2025-05-21 11:58:40 +0530

    Add text

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,6 +3,8 @@ import pandas as pd
 inputpath = r"data\Australian_Vehicle_Price_list.csv"
 outputpath = r"data\Australian_Vehicle_Price_list_v02.csv"
 
+# function here
+
 def preprocess_data(inputpath, outputpath):
     df = pd.read_csv(inputpath)
 
